cd3c84b171874dca3142741d3693661e8e82dd43
Cockong <45364934+Cockong@users.noreply.github.com>
2019-04-04 10:56:13 +0200

    Created the model of String.cpp, of test.cpp
    correct operator+

---
+++ String.cpp
@@ -0,0 +1,104 @@
+#include "String.h"
+
+
+
+  //Constructors
+  string::string(const string&)
+  {
+
+  }
+  string::string(char* p_str)
+  {
+
+  }
+
+  //Destructors
+  string::~string()
+  {
+
+  }
+
+  //Methods
+  char* string::c_str() //Convert to char array
+  {
+
+  }
+
+  void string::clear() //Reinitialisation of the String
+  {
+
+  }
+
+  void string::resize(int n) //Cut the String
+  {
+
+  }
+
+  bool string::empty() const  //Return if the size of the String is 0.
+  {
+
+  }
+
+  void string::reserve(size_t n) //Allocate the memory needed if n > capacity
+  {
+
+  }
+  void string::print()
+  {
+
+  }
+
+  //Getters
+  int string::size() //Return size
+  {
+
+  }
+
+  size_t string::length() //Return the size in bytes
+  {
+
+  }
+
+  size_t string::max_size() //Return the maximum potential length the string can reach due to known system or library in bytes
+                                      //implementation limitations
+  {
+
+  }
+
+  size_t capacity()//Return the current memory allocation for the String
+  {
+
+  }
+
+  //Operators
+  string& string::operator= (char c)
+  {
+
+  }
+
+  string& string::operator= (string& str)
+  {
+
+  }
+
+  string& string::operator= (char* p_c)
+  {
+
+  }
+
+  string operator+ (const char*   p_lhs, const string& rhs)
+  {
+
+  }
+
+  string operator+ (char          lhs, const string& rhs)
+  {
+
+  }
+
+  string operator+ (const string& lhs, const string& rhs)
+  {
+
+  }
+
+
--- String.h
+++ String.h
@@ -10,7 +10,7 @@ class string
     static  size_t MAX_SIZE;
     char* data;
     size_t capacity_;
-    const int char_size_;//1
+    static  size_t char_size_;//1
         
   public:
     //Constructors
@@ -27,28 +27,29 @@ class string
     void resize(int n); //Cut the String
     bool empty() const ; //Return if the size of the String is 0.
     void reserve(size_t n); //Allocate the memory needed if n > capacity
-		void print(); 
+    void print();
 
     //Getters
     int size(); //Return size
     size_t length(); //Return the size in bytes
-    static size_t max_size(); //Return the maximum potential length the string can reach due to known system or library in bytes
+    size_t max_size(); //Return the maximum potential length the string can reach due to known system or library in bytes
 					//implementation limitations
     size_t capacity();//Return the current memory allocation for the String   
 
     //Operators
-    string& operator=(char c);
-    string& operator=(string& str);
-    string& operator=(char* p_c);
-
-    string operator+ (const char*   p_lhs, const string& rhs);
-    string operator+ (char          lhs, const string& rhs);
-    string operator+ (const string& lhs, const string& rhs);
-
+    string& operator= (char c);
+    string& operator= (string& str);
+    string& operator= (char* p_c);
 
   private:
   
-}
+};
 #endif //STRING_H
+
+//Operators
+
+string operator+ (const char*   p_lhs, const string& rhs);
+string operator+ (char          lhs, const string& rhs);
+string operator+ (const string& lhs, const string& rhs);
 
 
