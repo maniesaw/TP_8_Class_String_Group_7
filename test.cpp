670a06f1c8ec0f148be678ba3320bad8951adfb4
Thiebaut Remi <remi.thiebaut@insa-lyon.fr>
2019-04-05 17:59:29 +0200

    made unitary test

--- test.cpp
+++ test.cpp
@@ -5,16 +5,15 @@ int main()
 {
   char p_str[]="hello";
   string mystring(p_str);
-  string mystring2(mystring);
 
   char p_strNull[] = "";
   string nullstring(p_strNull);
 
-  std::cout << mystring.capacity() << std::endl;
-  std::cout << mystring.empty() << std::endl;
+  std::cout << (mystring.capacity()==6) << std::endl;
+  std::cout << not mystring.empty() << std::endl;
 
-  std::cout << mystring.length() << std::endl;
-  std::cout << mystring.max_size() << std::endl;
+  std::cout << (mystring.length()==6) << std::endl;
+  std::cout << (mystring.max_size()==100) << std::endl;
 
 
 
@@ -38,12 +37,13 @@ int main()
   // test of operator =(char* p_c)
 
   string a = p_str;
-  std::cout << a.capacity() << std::endl;
-  std::cout << a.empty() << std::endl;
+  std::cout << (mystring.capacity()==a.capacity()) << std::endl;
+  std::cout << (mystring.empty()==a.empty()) << std::endl;
+  std::cout << (mystring.length()==a.length()) << std::endl;
  
   string b = p_strNull;
-  std::cout << b.capacity() << std::endl;
-  std::cout << b.empty() << std::endl;
+  std::cout << (b.capacity()==1) << std::endl;
+  std::cout << (b.empty()==1) << std::endl;
 
   return 0;
 }
